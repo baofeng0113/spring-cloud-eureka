a1f4d8d458ac5d5782f8ce09d127fee5fee7b1ab
baofeng0113 <baofeng0113@gmail.com>
2018-04-28 20:55:22 +0800

    init

---
+++ src/main/java/cloud/dispatcher/midware/eurekaserver/Application.java
@@ -0,0 +1,21 @@
+package cloud.dispatcher.midware.eurekaserver;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class Application {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+
+	public static void main(String[] args) {
+        long currentTimeMillis = System.currentTimeMillis();
+		SpringApplication.run(Application.class, args);
+        LOGGER.info("Spring boot application startup, cost: {}ms",
+                System.currentTimeMillis() - currentTimeMillis);
+	}
+}
